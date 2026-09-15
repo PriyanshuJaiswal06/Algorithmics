e0bda555f4de729e572dad284a7074f91857bfbc
Priyanshu Jaiswal <jaiswalpriyanshu792@gmail.com>
2026-09-15 13:39:33 +0530

    Word Capitalization

---
+++ Codeforces/281A.py
@@ -0,0 +1,8 @@
+# A. Word Capitalization
+# Capitalization is writing a word with its first letter as a capital letter. Your task is to capitalize the given word.
+
+# Note, that during capitalization all the letters except the first one remains unchanged.
+
+
+word = input()
+print(word[0].capitalize()+word[1:])
